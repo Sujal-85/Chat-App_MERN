2664f36f122484f1a4a69c22c70da464023236ab
Sujal sadanand khedekar <khedekarsujay720@gmail.com>
2025-03-28 21:43:36 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import {io} from "socket.io-client";
 
-const Base_url = "http://localhost:5001";
+const Base_url = "https://chat-app-mern-mq93.onrender.com";
 export const useAuthStore = create((set,get) => ({
 
     authUser: null,
